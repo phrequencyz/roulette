28308c836a1f45d3d39ab445e02122a752bea6d8
phrequencyz <phrequencyz@gmail.com>
2026-02-05 12:06:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ JSON_URL = "https://raw.githubusercontent.com/phrequencyz/roulette/refs/heads/ma
 def get_prizes():
     try:
         r = requests.get(JSON_URL, timeout=5)
-        r.raise_for_status() # Проверит, что ссылка открылась успешно
-        return r.json()
+        r.raise_for_status()
+        return r.json(), False
     except Exception as e:
         print(f"Ошибка загрузки JSON: {e}")
-        # Если не загрузилось, отдаем стандартный набор, чтобы код не падал
-        return [{"name": "Ошибка ссылки", "chance": 100}]
+        # Возвращаем дефолтный набор и флаг ошибки True
+        return [{"name": "Ошибка данных", "chance": 100}], True
 
 @app.route('/')
 def index():
@@ -23,17 +23,19 @@ def index():
 
 @app.route('/spin', methods=['POST'])
 def spin():
-    prizes = get_prizes()
+    prizes, is_error = get_prizes()
     names = [p['name'] for p in prizes]
     weights = [p['chance'] for p in prizes]
     
-    # Выбираем приз
     selected_prize = random.choices(names, weights=weights, k=1)[0]
     
     return jsonify({
         "prize": selected_prize,
         "index": names.index(selected_prize),
         "total_segments": len(names),
-        "all_names": names
+        "all_names": names,
+        "error": is_error
     })
 
+if __name__ == '__main__':
+    app.run()
